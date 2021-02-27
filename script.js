606b22b1400068951f7839b8cd545ad6f9a388ca
bezustally <bezustally@gmail.com>
2021-02-27 06:18:20 +0300

    v.2.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const imageContainer = document.getElementById('image-container')
-const loader = document.getElementById('loader')
+const imageContainer = document.getElementById('image-container');
+const loader = document.getElementById('loader');
 
 let ready = false;
 let imagesLoaded = 0;
@@ -7,9 +7,9 @@ let totalImages = 0;
 let photosArray = [];
 
 // Unsplash API
-const count = 10;
-const apiKey = '0Wo1lKytfk9D8qsGJWJCdxtS6uFmP2eEp3nvAne3Wp4'; 
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=space`;
+const count = 5;
+const apiKey = '0Wo1lKytfk9D8qsGJWJCdxtS6uFmP2eEp3nvAne3Wp4';
+const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}&query=universe`;
 
 // Check if all images were loaded
 function imageLoaded() {
@@ -53,8 +53,8 @@ function displayPhotos() {
         img.addEventListener('load', imageLoaded);
         // Put <img> inside <a>, then put both inside imageContainer Element
         item.appendChild(img);
-        imageContainer.appendChild(item);        
-    }) 
+        imageContainer.appendChild(item);
+    });
 }
 
 // Get Photos From API
@@ -64,7 +64,7 @@ async function getPhotos() {
         photosArray = await response.json();
         displayPhotos();
     } catch (error) {
-    // Catch Error Here
+        // Catch Error Here
     };
 }
 
